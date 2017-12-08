409a64e6b08158418cd039a6aa80113fe3bd570a
Bo Lu <lv.patrick@gmail.com>
2017-12-08 17:26:14 +1100

    add utf-8 path example, fix #2

---
+++ examples/utf8.rs
@@ -0,0 +1,20 @@
+extern crate zbox;
+
+use zbox::{init_env, RepoOpener};
+
+fn main() {
+    // initialise zbox environment, called first
+    init_env();
+
+    // create and open a repository
+    let mut repo = RepoOpener::new()
+        .create(true)
+        .open("mem://utf8", "your password")
+        .unwrap();
+
+    repo.create_dir("/Hello").unwrap();
+    repo.create_dir("/你好").unwrap();
+    repo.create_dir("/こんにちは").unwrap();
+    repo.create_dir("/안녕하세요").unwrap();
+    repo.create_dir("/Здравствуйте").unwrap();
+}
