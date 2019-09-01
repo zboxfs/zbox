3ce7ddde4aed863702a80b1ef61cbff6aeeb6341
Bo Lu <lv.patrick@gmail.com>
2019-09-02 09:10:59 +1000

    remove unused vio.rs file from file storage

--- src/volume/storage/file/vio.rs
+++
@@ -1,8 +0,0 @@
-//! Virtual IO
-//!
-//! This module is to provide a zero-cost abstraction for OS file system API.
-
-pub use std::fs::{
-    copy, create_dir, create_dir_all, metadata, read_dir, remove_dir,
-    remove_dir_all, remove_file, rename, File, OpenOptions, ReadDir,
-};
